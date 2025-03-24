e957d682d49d039f439d22068bdda26ae073cca0
Felipe Roiko Felix da Silva <roiko2010@gmail.com>
2025-03-24 07:14:44 -0300

    finalizado
    add repositorio teste com documentação

--- src/transcription/transc_endpoint.py
+++ src/transcription/transc_endpoint.py
@@ -3,11 +3,6 @@ from .transc_controller import delete_transcription, get_all_transcriptions, get
 
 transcription_bp = Blueprint('transcription', __name__)
 
-# transcription_bp.route('/transcribe', methods=['POST'])(transcribe)
-# transcription_bp.route('/transcriptions', methods=['GET'])(get_all_transcriptions)
-# transcription_bp.route('/transcriptions/<transcription_id>', methods=['GET'])(get_transcription_id)
-# transcription_bp.route('/transcriptions/<transcription_id>', methods=['PUT'])(update_transcription)
-# transcription_bp.route('/transcriptions/<transcription_id>', methods=['DELETE'])(delete_transcription)
 print("Blueprint transcription_bp registrado")
 transcription_bp.route('/transcribe', methods=['POST'])(transcribe)
 transcription_bp.route('/', methods=['GET'])(get_all_transcriptions)
